ca93f247d4fe979908b9cd24db13e5303877a6e9
AbhishekMaraboina <abhishekdv20@gmail.com>
2021-12-22 16:57:55 +0530

    updated v5

--- ec2-cancel.sh
+++ ec2-cancel.sh
@@ -17,19 +17,16 @@ fi
 CANCEL_INSTANCE() {
   ## Check if instance is already there
 
-  SPOTINSTID=$(aws ec2 describe-spot-instance-requests --filters "Name=tag:Name,Values=${COMPONENT}" | jq .SpotInstanceRequests[].SpotInstanceRequestId)
+  spotinsid=$(aws ec2 describe-spot-instance-requests --filters "Name=tag:Name,Values=${COMPONENT}" | jq .SpotInstanceRequests[].SpotInstanceRequestId | sed 's/"//g' | grep -v null)
 
   aws ec2 describe-spot-instance-requests --filters "Name=tag:Name,Values=${COMPONENT}" | jq .SpotInstanceRequests[].State | sed 's/"//g' | grep -E 'active'
   if [ $? -eq -0 ]; then
-    aws ec2 cancel-spot-instance-requests --spot-instance-request-ids "SPOTINSTID"
+    aws ec2 cancel-spot-instance-requests --spot-instance-request-ids ${SPOTINSTID}
   else
     echo -e "\e[1;33mInstance is already cancelled\e[0m"
   fi
 
   sleep 10
-
-
-
 }
 
 if [ "$COMPONENT" == "all" ]; then
