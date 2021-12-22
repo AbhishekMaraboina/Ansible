edc8c19275cb8045f72dcfaf65d333c230ec1610
AbhishekMaraboina <abhishekdv20@gmail.com>
2021-12-22 16:50:16 +0530

    updated v3

--- ec2-cancel.sh
+++ ec2-cancel.sh
@@ -20,9 +20,8 @@ CANCEL_INSTANCE() {
   SPOTINSTID=$(aws ec2 describe-spot-instance-requests --filters "Name=tag:Name,Values=${COMPONENT}" | jq .SpotInstanceRequests[].SpotInstanceRequestId | sed 's/"//g' | grep -v null)
 
   aws ec2 describe-spot-instance-requests --filters "Name=tag:Name,Values=${COMPONENT}" | jq .SpotInstanceRequests[].State | sed 's/"//g' | grep -E 'active'
-   echo $(SPOTINSTID)
   if [ $? -eq -0 ]; then
-    aws ec2 cancel-spot-instance-requests --spot-instance-request-ids $(SPOTINSTID)
+    aws ec2 cancel-spot-instance-requests --spot-instance-request-ids SPOTINSTID
   else
     echo -e "\e[1;33mInstance is already cancelled\e[0m"
   fi
